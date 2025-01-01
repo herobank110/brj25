13c2c8e6d4da666c4bcb1a313c5bfcb6eb1b6663
David Kanekanian <dkanekanian1@gmail.com>
2025-01-01 17:49:08 +0000

    save

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,89 @@
-import { Application, Assets, Sprite } from 'pixi.js';
+import { Application, Assets, Container, Sprite } from "pixi.js";
 
-(async () =>
-{
-    // Create a new application
-    const app = new Application();
+// This example is the based on basic/container, but using OffscreenCanvas.
 
-    // Initialize the application
-    await app.init({ background: '#1099bb', resizeTo: window });
+const canvas = document.createElement("canvas");
+// const view = canvas.transferControlToOffscreen();
 
-    // Append the application canvas to the document body
-    document.body.appendChild(app.canvas);
+(async () => {
+  // Create a new application
+  const app = new Application();
 
-    // Load the bunny texture
-    const texture = await Assets.load('https://pixijs.com/assets/bunny.png');
+  // Initialize the application
+  await app.init({
+    canvas,
+    background: "#1099bb", //resizeTo: window
+    width: 100,
+    height: 100,
+  });
 
-    // Create a bunny Sprite
+  // Append the application canvas to the document body
+  //   document.body.appendChild(canvas);
+
+  const container = new Container();
+
+  app.stage.addChild(container);
+
+  // Load the bunny texture
+  const texture = await Assets.load("https://pixijs.com/assets/bunny.png");
+
+  // Create a 5x5 grid of bunnies
+  for (let i = 0; i < 25; i++) {
     const bunny = new Sprite(texture);
 
-    // Center the sprite's anchor point
     bunny.anchor.set(0.5);
+    bunny.x = (i % 5) * 40;
+    bunny.y = Math.floor(i / 5) * 40;
+    container.addChild(bunny);
+  }
 
-    // Move the sprite to the center of the screen
-    bunny.x = app.screen.width / 2;
-    bunny.y = app.screen.height / 2;
+  // Move container to the center
+  container.x = app.screen.width / 2;
+  container.y = app.screen.height / 2;
 
-    app.stage.addChild(bunny);
+  // Center bunny sprite in local container coordinates
+  container.pivot.x = container.width / 2;
+  container.pivot.y = container.height / 2;
 
-    // Listen for animate update
-    app.ticker.add((time) =>
-    {
-        // Just for fun, let's rotate mr rabbit a little.
-        // * Delta is 1 if running at 100% performance *
-        // * Creates frame-independent transformation *
-        bunny.rotation += 0.1 * time.deltaTime;
-    });
+  // Listen for animate update
+  app.ticker.add((time) => {
+    // Rotate the container!
+    // * use delta to create frame-independent transform *
+    container.rotation -= 0.01 * time.deltaTime;
+  });
 })();
+
+import * as THREE from "three";
+
+const width = window.innerWidth,
+  height = window.innerHeight;
+
+// init
+
+const camera = new THREE.PerspectiveCamera(70, width / height, 0.01, 10);
+camera.position.z = 1;
+
+const scene = new THREE.Scene();
+
+const geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
+const material = new THREE.MeshBasicMaterial();
+
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(width, height);
+renderer.setAnimationLoop(animate);
+document.body.appendChild(renderer.domElement);
+
+// animation
+
+function animate(time) {
+  mesh.rotation.x = time / 2000;
+  mesh.rotation.y = time / 1000;
+
+  if (!material.map) material.map = new THREE.CanvasTexture(canvas);
+  material.map.needsUpdate = true;
+
+  renderer.render(scene, camera);
+}
